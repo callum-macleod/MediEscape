4f99d702e663c5e6f4f0153292227a51625995bb
beaniebhoy <beaniebhoy03@gmail.com>
2025-03-18 11:44:50 +0000

    health bar now updates live

--- Assets/_Scripts/HealthMgr.cs
+++ Assets/_Scripts/HealthMgr.cs
@@ -14,6 +14,11 @@ public class HealthMgr : MonoBehaviour
         DrawHearts();
     }
 
+    public void Update()
+    {
+        DrawHearts();
+    }
+
     public void DrawHearts()
     {
         ClearHearts();
