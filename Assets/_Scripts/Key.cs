e066f2f039e7997beefbb500e42fd109e010966f
callum-macleod <macleodcallum27@gmail.com>
2025-03-31 14:08:53 +0100

    Kal - intermediary stuff

--- Assets/_Scripts/Key.cs
+++ Assets/_Scripts/Key.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 using static UnityEditor.Experimental.GraphView.GraphView;
 
 [CreateAssetMenu(fileName = "Key", menuName = "Item/Key")]
-public class Key : Item
+public class Key : ItemInfo
 {
     [SerializeField] Thief player;
-    public override void Use()
+    public void Use()
     {
-        base.Use();
+        //base.Use();
 
         Collider2D door = Physics2D.OverlapCircle(player.transform.position, 2, 1 << (int)Layers.Door);
         if (door.TryGetComponent<UnlockableDoor>(out UnlockableDoor doorScript))
