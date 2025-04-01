a8b5222112d4c5d22c4db6c074849ad58cca45c4
beaniebhoy <beaniebhoy03@gmail.com>
2025-04-01 14:13:58 +0100

    added pause functionality

---
+++ Assets/_Scripts/PauseGameMgr.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PauseGameMgr : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    [SerializeField] private GameObject pauseMenuUI; // Optional UI panel
+
+    private void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+
+        if (pauseMenuUI != null)
+            pauseMenuUI.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        if (pauseMenuUI != null)
+            pauseMenuUI.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+    }
+}
