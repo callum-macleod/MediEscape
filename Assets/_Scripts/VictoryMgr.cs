578a38eac14461345a583cd4dc235c2b24c4d29c
beaniebhoy <beaniebhoy03@gmail.com>
2025-04-03 10:35:05 +0100

    win condition added
    win condition added with screen and logic

---
+++ Assets/_Scripts/VictoryMgr.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryMgr : MonoBehaviour
+{
+
+    private GameObject player;
+    [SerializeField] GameObject victoryscreenUI;
+
+
+    public static VictoryMgr Instance { get; private set; }
+
+    //void Awake()
+    //{
+    //    if (Instance == null)
+    //        Instance = this;
+    //    else
+    //        victoryscreenUI.SetActive(false);
+
+    //    //victoryscreenUI.SetActive(false);
+    //}
+
+    public void ShowVictoryScreen()
+    {
+        victoryscreenUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        victoryscreenUI.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player = collision.gameObject;
+            if (collision.CompareTag("Player"))
+            {
+                ShowVictoryScreen();
+            }
+        }
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
+    }
+}
+
